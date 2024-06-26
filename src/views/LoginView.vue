43b2e4b000d17c09a7581eed433e8d3c3084e4a1
roberthdanielt <roberthdanielt@gmail.com>
2024-06-26 12:04:45 -0500

    update example

---
+++ src/views/LoginView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+      <h2>Home</h2>
+      <img alt="Vue logo" src="../assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  
+  
+  export default {
+    name: 'HomeView',
+    components: {
+      
+    }
+  }
+  </script>
+  
