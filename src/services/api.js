9a592bbb671911a5bc36e996038c9dc4b55b135e
roberthdanielt <roberthdanielt@gmail.com>
2024-11-06 05:12:01 -0500

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const api = axios.create({
     //baseURL: 'http://localhost:8081/api'
-    baseURL: `${process.env.VUE_APP_API_BASE_URL}/api`,
+    baseURL: `${process.env.VUE_APP_API_BASE_URL}`,
     timeout: 15000,
     headers: {
         'Content-Type': 'application/json'
